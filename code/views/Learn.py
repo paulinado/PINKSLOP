ae80b4d6d2581aedb9665b21318370ef46e3fdb3
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-29 00:25:51 +0100

    Quiz quesiton start

--- code/views/Learn.py
+++ code/views/Learn.py
@@ -3,22 +3,61 @@ from PIL import Image
 import path
 import sys
 
+QUESTIONS_NUMBER = 10
+
+dir = path.Path(__file__).abspath().parent.parent.parent
+sys.path.append(dir.parent.parent.parent)
+
 def click_button():
     st.session_state.clicked = True
 
+def get_next_question():
+    pass
+
+def prepare_question(question_number):
+    correct = False
+    f = open(dir+'/code/questions.txt', 'r')
+    lines = f.readlines()
+    q = lines[0]
+    a = lines[1]
+    while st.session_state.current_question <= QUESTIONS_NUMBER:
+        placeholder = st.empty()
+        with placeholder.container():
+            st.write("Question", str(question_number))
+            st.write(q)
+
+            form = st.form("Answer")
+            answer = form.text_input("Enter your answer here")
+            submitted = form.form_submit_button("Check")
+            while not submitted:
+                continue
+            else:
+                if answer == a:
+                    form.write("Correct!")
+                    correct = True
+                    st.session_state.current_question += 1
+                else:
+                    form.write("Incorrect! Try again")
+                    correct = False
+                placeholder.empty()
+    f.close()
+    return correct
+
+
 def start_quiz():
+    if 'current_question' not in st.session_state:
+        st.session_state.current_question = 1
     level=0
     num = level/7
     string = "Level "+str(level)
     st.progress(num, string)
-        
+    prepare_question(st.session_state.current_question)
+
+
 def createPage():
     st.title("Learn")
     col1, col2, col3 = st.columns([1, 2, 1])
 
-    dir = path.Path(__file__).abspath().parent.parent.parent
-    sys.path.append(dir.parent.parent.parent)
-    
     if 'clicked' not in st.session_state:
         st.session_state.clicked = False
     
@@ -31,5 +70,5 @@ def createPage():
         button = st.empty()
         click = button.button("Begin Learning 🎉", on_click=click_button)
         if st.session_state.clicked:
-            start_quiz()
             button.empty()
+            start_quiz()

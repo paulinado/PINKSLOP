ac45a843faf5e5d773d68c5024e9d530a5b21366
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-28 16:30:52 +0100

    Update path

--- code/pages/Learn.py
+++ code/pages/Learn.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from PIL import Image
+import path
+import sys
 
 level=0
 col1, col2, col3 = st.columns([1, 2, 1])
+
+dir = path.Path(__file__).abspath()
+sys.path.append(dir.parent.parent)
 
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
@@ -15,7 +20,8 @@ def start_quiz():
     st.progress(level/7, "Level "+str(level))
 
 with col3:
-    image = Image.open('./elements/polly_pig.jpg')
+    path_to_image = "./../elements/polly_pig.jpg"
+    image = Image.open(path_to_image)
     st.image(image=image)
 
 with col2:

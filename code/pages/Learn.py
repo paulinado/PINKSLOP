f0bed139bf67137020d1bd5ed329d1990e0f0298
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-28 15:53:29 +0100

    Add start learning button

--- code/pages/Learn.py
+++ code/pages/Learn.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.header("Learn")
+level=0
+col1, col2, col3 = st.columns([1, 2, 1])
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+def start_quiz():
+    st.header("Learn")
+    st.progress(level/7, "Level "+str(level))
+
+with col2:
+    button = st.empty()
+    click = button.button("Begin Learning 🎉", on_click=click_button)
+    if st.session_state.clicked:
+        start_quiz()
+        button.empty()

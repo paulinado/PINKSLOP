458533d55ad9abaed9231bbadbbae356784aaf3d
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-28 13:58:05 +0100

    Add tabs

--- code/main.py
+++ code/main.py
@@ -1,5 +1,12 @@
 import streamlit as st
  
-st.write("""
-# Maths with Polly the Pig
-""")
+learn, progress, settings = st.tabs(["Learn", "Progress", "Settings"])
+
+with learn:
+    st.header("Learn")
+
+with progress:
+    st.header("Progress")
+
+with settings:
+    st.header("Settings")

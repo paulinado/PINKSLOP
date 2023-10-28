f4045ab2dff840cf4098f2bab6b5f843aa8d5a32
Olivia Borgstrom <olivia.borgstrom@student.manchester.ac.uk>
2023-10-28 16:30:45 +0100

    testing

--- code/main.py
+++ code/main.py
@@ -1,16 +1,10 @@
 import streamlit as st
 import pandas as pd
 from auth0_component import login_button
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+st.header("Home")
 
 clientId = "cONtS7pEn44avXLyIsdJngqkDu0bwGqj"
 domain = "dev-ep7cvhbpossvx8o6.uk.auth0.com"
 
 user_info = login_button(clientId, domain = domain)
-st.write(user_info)
- 
-st.header("Home")

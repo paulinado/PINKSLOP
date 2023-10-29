0a4c79f091c5b9cb3e7181712024ab33e66c4da4
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-29 08:35:10 +0000

    Get data from database

--- code/views/Progress.py
+++ code/views/Progress.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from deta import Deta
+import numpy as np
 
-def get_user_data():
-    correct = {"Levels": [1, 2, 3], "Correct answers": [8, 5, 10]}
-    return correct
+def get_user_data(key):
+    deta = Deta(st.secrets["data_key"])
+    db = deta.Base("progress-new")
+    user = db.get(key)
+    level = user['level']
+    levels = np.arrange(1, level+1)
+    first_time_correct = user['first_time_correct']
+    data = {'Levels' : levels, 'Correct answers': first_time_correct}
+    return data
 
-def createPage():
+def createPage(key):
     st.title("Progress")
     st.text("Check out your learning progress so far!")
-    correct_answers = pd.DataFrame(get_user_data(), columns=["Levels", "Correct answers"])
+    correct_answers = pd.DataFrame(get_user_data(key), columns=["Levels", "Correct answers"])
     fig, ax = plt.subplots()
     ax.plot(correct_answers["Levels"], correct_answers["Correct answers"], color="#5069af")
     ax.set_title("Questions answered correctly on first try")

f47d3d2bc5506c6671f3af703dca70462538ffc6
Luke Sanderson <lksndrsn@gmail.com>
2023-10-29 01:41:57 +0000

    Added numPerView setting

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import streamlit_authenticator as stauth
 from streamlit_option_menu import option_menu
 import pickle
 from pathlib import Path
-from views import Learn, Progress, Settings, ExampleDB, Learn2
+from views import Learn, Progress, Settings, Learn2
 import re
 from deta import Deta
 
@@ -62,7 +62,11 @@ def sign_up():
         else:
             st.warning('Invalid Username')
 
-        
+if "username" not in st.session_state:
+    st.session_state.username = ""
+if "numPerLevel" not in st.session_state:
+    st.session_state.numPerLevel = 10
+
         
 deta = Deta(st.secrets["data_key"])
 db = deta.Base("users")
@@ -93,6 +97,7 @@ try:
         if username in usernames:
             if authentication_status:
                 # let User see app
+                st.session_state.username = username
                 st.sidebar.subheader(f'Welcome {username}')
                 authenticator.logout('Log Out', 'sidebar')
                 
@@ -115,11 +120,8 @@ try:
                     Progress.createPage()
                 
                 if selected=="Settings":
-                    Settings.createPage()
+                    Settings.createPage(username)
                 
-                if selected=="ExampleDB":
-                    ExampleDB.createPage(deta)
-
             elif not authentication_status:
                 with info:
                     st.error('Incorrect Password or username')

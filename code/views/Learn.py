6232cecd6d4c03a5b0893fefdc5640bc857601ab
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-10-29 01:42:38 +0100

    Add prev and next buttons

--- code/views/Learn.py
+++ code/views/Learn.py
@@ -13,65 +13,69 @@ def click_button():
     st.session_state.clicked = True
 
 def get_next_question():
-    pass
-
-def prepare_question():
-    correct = False
     f = open(dir+'/code/questions.txt', 'r')
     lines = f.readlines()
-    while st.session_state.current_question <= QUESTIONS_NUMBER:
-        q = lines[st.session_state.current_question*2].strip()
-        a = lines[st.session_state.current_question*2+1].strip()
-        placeholder = st.empty()
-        with placeholder.container():
-            st.write("Question", str(st.session_state.current_question + 1))
-            st.write(q)
-            st.write(a)
-            form = st.form("Answer")
-            answer = form.text_input("Enter your answer here")
-            submitted = form.form_submit_button("Check")
-            while not submitted:
-                continue
-            else:
-                if answer == a:
-                    form.write("Correct!")
-                    time.sleep(5)
-                    correct = True
-                    st.session_state.current_question += 1
-                else:
-                    form.write("Incorrect! Try again")
-                    time.sleep(5)
-                    correct = False
-                placeholder.empty()
+    q = lines[st.session_state.current_question*2].strip()
+    a = lines[st.session_state.current_question*2+1].strip()
     f.close()
-    return correct
+    return (q, a)
 
+def display_question():
+    q, a = get_next_question()
 
-def start_quiz():
-    if 'current_question' not in st.session_state:
-        st.session_state.current_question = 0
-    level=0
-    num = level/7
-    string = "Level "+str(level)
-    st.progress(num, string)
-    prepare_question()
+    if st.session_state.current_question in st.session_state.answers:
+        index = st.session_state.answers[st.session_state.current_question]
 
+    st.write("Question", str(st.session_state.current_question + 1))
+    st.write(q)
+    form = st.form("Answer", clear_on_submit=True)
+    answer = form.text_input("Enter your answer here")
+    submitted = form.form_submit_button("Check")
+    if submitted:
+        st.session_state.answers[st.session_state.current_question] = answer
+        if answer == a:
+            form.write("Correct!")
+        else:
+            form.write("Incorrect! Try again")
+
+def prev_question():
+    if st.session_state.current_question > 0:
+        st.session_state.current_question -= 1
+
+def next_question():
+    st.session_state.current_question += 1
 
 def createPage():
     st.title("Learn")
     col1, col2, col3 = st.columns([1, 2, 1])
+    level=0
+    num = level/7
+    string = "Level "+str(level)
 
+    if 'current_question' not in st.session_state:
+        st.session_state.current_question = 0
+    if 'questions' not in st.session_state:
+        st.session_state.questions = []
+    if 'answers' not in st.session_state:
+        st.session_state.answers = {}
     if 'clicked' not in st.session_state:
         st.session_state.clicked = False
+    
+    with col1:
+        if col1.button("Prev"):
+            prev_question()
     
     with col3:
         path_to_image = dir + "/elements/polly_pig.jpg"
         image = Image.open(path_to_image)
         st.image(image=image)
+        if col3.button("Next"):
+            next_question()
 
     with col2:
         button = st.empty()
         click = button.button("Begin Learning 🎉", on_click=click_button)
         if st.session_state.clicked:
             button.empty()
-            start_quiz()
+            st.progress(num, string)
+            display_question()
